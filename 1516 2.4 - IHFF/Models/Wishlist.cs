f5144b0b34c0a9a790d1a1120c3c4b32c4421e8d
geoffrey <geoffreyarkenbout@gmail.com>
2016-05-03 23:22:37 +0200

    Fix for models (missing primary key)
    Trying to get nav selection to work

--- 1516 2.4 - IHFF/Models/Wishlist.cs
+++ 1516 2.4 - IHFF/Models/Wishlist.cs
@@ -10,6 +10,7 @@ namespace IHFF.Models
     [Table("Wishlists")]
     public class Wishlist
     {
+        [Key]
         public int Id { get; set; }
         public string EmailAddress { get; set; }
         public string Name { get; set; }
--- 1516 2.4 - IHFF/Models/WishlistItem.cs
+++ 1516 2.4 - IHFF/Models/WishlistItem.cs
@@ -6,8 +6,13 @@ namespace IHFF.Models
     [Table("WishlistItems")]
     public class WishlistItem
     {
+        [Key, Column(Order = 0)]
+        public int Wishlist_Id { get; set; }
         public virtual Wishlist Wishlist { get; set; }
+        [Key, Column(Order = 1)]
         public int Item_Id { get; set; }
+        [Key, Column(Order = 2)]
+        public int ItemType { get; set; }
         public int Amount { get; set; }
         public bool Reserved { get; set; }
     }
