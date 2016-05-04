00447e38bab6d955a5ff7bdaea046aa73c76f190
kursun12 <salih_kara@hotmail.nl>
2016-05-04 23:09:40 +0200

    getting in

---
+++ 1516 2.4 - IHFF/Controllers/RestaurantController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IHFF.Repositories;
+using IHFF.Interfaces;
+
+namespace IHFF.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
