589916776536a79690313f83aa1c55069adeb0e2
kursun12 <salih_kara@hotmail.nl>
2016-05-30 13:12:08 +0200

    restaurant to wishlisht working on

--- 1516 2.4 - IHFF/Controllers/RestaurantController.cs
+++ 1516 2.4 - IHFF/Controllers/RestaurantController.cs
@@ -12,10 +12,30 @@ namespace IHFF.Controllers
     public class RestaurantController : Controller
     {
         private IRestaurantRepository restaurantsRepository = new RestaurantsRepository();
-        
+        private IWishlistRepository wishlistRepository = new WishlistRepository();
+
         public ActionResult Index()
         {
            return View(restaurantsRepository.GetAllRestaurants());
+        }
+
+        [HttpPost]
+        public ActionResult Index(int Datum, int Tijd, int AantalPersonen)
+        {
+            Wishlist wishlist = wishlistRepository.GetOrCreateWishlist(Wishlist.Instance.UID);
+            // Event e = restaurantsRepository.GetAllRestaurants().First(x => x.EventId == EventId) as Event;
+
+            WishlistItem item = new WishlistItem();
+            //item.Event = e;
+            item.Wishlist = wishlist;
+            item.Amount = AantalPersonen;
+            //item.EventId = e.EventId;
+            item.PayedFor = false;
+
+            wishlist.WishlistItems.Add(item);
+
+            Wishlist.Instance = wishlist;
+            return RedirectToAction("Index", "Wishlist");
         }
 
         public ActionResult RestaurantInfo(int Id)
